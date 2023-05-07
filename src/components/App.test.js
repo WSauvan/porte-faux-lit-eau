582cfb16e2bd5cf66db26060c1a96a2008057690
wsauvan <wsauvan@webqam.fr>
2023-05-07 23:56:11 +0200

    feat(structure): app testing improved

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,25 +1,14 @@
 import { render, screen, act } from '@testing-library/react';
-import { unmountComponentAtNode } from 'react-dom';
 import App from './App';
 import React from 'react';
 
-let container = null;
-beforeEach(() => {
-    // setup a DOM element as a render target
-    container = document.createElement('div');
-    document.body.appendChild(container);
-});
+describe('<App />', () => {
+    test('it should mount', async () => {
+        await act(async () => {
+            render(<App />);
+        });
 
-afterEach(() => {
-    // cleanup on exiting
-    unmountComponentAtNode(container);
-    container.remove();
-    container = null;
-});
-
-test('it should mount', async () => {
-    await act(async () => {
-        render(<App />, container);
+        const appComponent = screen.getByTestId('App');
+        expect(appComponent).toBeInTheDocument();
     });
-    expect(container).toBeInTheDocument();
 });
