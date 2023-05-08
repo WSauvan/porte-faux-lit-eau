c2a6b3e0900937b6779121fbbfaf348efe0dde40
wsauvan <wsauvan@webqam.fr>
2023-05-08 23:04:27 +0200

    feat(structure): styled navbar

--- src/components/layout/NavBar/NavBar.js
+++ src/components/layout/NavBar/NavBar.js
@@ -2,13 +2,21 @@ import React from 'react';
 import styles from './NavBar.module.scss';
 import { NavLink } from 'react-router-dom';
 
+const navbarActiveCheckboxId = 'navbaractive';
+const getLinkClass = ({ isActive }) =>
+    isActive
+        ? styles.NavBar_link + ' ' + styles.NavBar_link___active
+        : styles.NavBar_link;
+const onNavLinkClick = () => {
+    document.getElementById(navbarActiveCheckboxId).checked = false;
+};
 const NavBar = () => {
     return (
         <div className={styles.NavBar} data-testid="NavBar">
             <input
                 type="checkbox"
                 className={styles.NavBar_activeInput}
-                id="navbaractive"
+                id={navbarActiveCheckboxId}
             />
             <label htmlFor="navbaractive" className={styles.NavBar_activeLabel}>
                 <i className="fas fa-bars"></i>
@@ -16,41 +24,29 @@ const NavBar = () => {
             <div className={styles.NavBar_wrapper}>
                 <NavLink
                     to="/"
-                    className={({ isActive }) =>
-                        isActive
-                            ? styles.NavBar_link___active
-                            : styles.NavBar_link
-                    }
+                    className={getLinkClass}
+                    onClick={onNavLinkClick}
                 >
-                    Home
+                    <label htmlFor="navbaractive">Home</label>
                 </NavLink>
                 <NavLink
                     to="/contact"
-                    className={({ isActive }) =>
-                        isActive
-                            ? styles.NavBar_link___active
-                            : styles.NavBar_link
-                    }
+                    className={getLinkClass}
+                    onClick={onNavLinkClick}
                 >
                     Contact
                 </NavLink>
                 <NavLink
                     to="/projects"
-                    className={({ isActive }) =>
-                        isActive
-                            ? styles.NavBar_link___active
-                            : styles.NavBar_link
-                    }
+                    className={getLinkClass}
+                    onClick={onNavLinkClick}
                 >
                     Projects
                 </NavLink>
                 <NavLink
                     to="/skills"
-                    className={({ isActive }) =>
-                        isActive
-                            ? styles.NavBar_link___active
-                            : styles.NavBar_link
-                    }
+                    className={getLinkClass}
+                    onClick={onNavLinkClick}
                 >
                     Skills
                 </NavLink>
